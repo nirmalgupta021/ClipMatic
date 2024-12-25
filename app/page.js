18f5023831ff41475e2edf7f4c585f36c525f7a9
The-ZeGOD <nihalgupta.nirmal@gmail.com>
2024-12-25 19:15:13 +0530

    Done Authentication using clerk

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+  
+
   return (
     <div>
       <h2>Hiii</h2>
       <Button>Hello</Button>
+      <UserButton/>
     </div>
   );
 }
